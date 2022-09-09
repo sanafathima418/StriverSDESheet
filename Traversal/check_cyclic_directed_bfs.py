4924cb1c42cdb6bf5f5da0d72dc664ff89b3abfe
Sana Fathima <sana.fathima@colorado.edu>
2022-09-08 20:52:55 -0600

    Check Cycle

--- Traversal/check_cyclic_directed_bfs.py
+++ Traversal/check_cyclic_directed_bfs.py
@@ -11,21 +11,51 @@ def detectCycleInDirectedGraph(n, edges):
     # Indegree array
     indegree = [0] * n
     
+    # BFS Queue
+    bfs_queue = []
+    
+    # Traversal Count variable instead of Topo Sort Order Array (Same purpose as we only need length)
+    traversal_count = 0
+    
     # Populate Adjacency list for all edges
     for i in range(len(edges)):
         adj_list[edges[i][0]].append(edges[i][1])
         # Update Indegree while creating adj list
         indegree[edges[i][1] - 1] += 1
         
-    # Add nodes that don't have 0 outdegree and indegree : no edges
+     # Add nodes that don't have 0 outdegree and indegree : no edges
     for i in range(n):
         if not adj_list[i]:
             adj_list[i] = []
-    
+        
+    # BFS Traversal
+    def bfs_traversal():
+        nonlocal traversal_count
+        
+        # 1. While queue not empty, process all nodes
+        while(bfs_queue):
+            # 2. Pop from Queue and increment traversal count
+            curr_node = bfs_queue.pop(0)
+            traversal_count += 1
+            # 3. For every node adjacent to current node
+            for adj_node in adj_list[curr_node]:
+                # 4. Reduce Indegree
+                indegree[adj_node - 1] -= 1
+                if indegree[adj_node - 1] == 0:
+                    # 5. If indegree == 0 or less, Push to Queue
+                    bfs_queue.append(adj_node)
+        
     # Start Point: Get nodes with indegree = 0 
-    for i in range(1,n):
-        if indegree[i-1] == 0:
-            return 0
+    for i in range(n):
+        if indegree[i - 1] == 0:
+            # Append to Queue
+            bfs_queue.append(i)
+    
+    bfs_traversal() 
+    
+    # If topo sort happens for all nodes then no cycle exists, else cycle exists 
+    if traversal_count == n:
+        return 0
     return 1
     
     
