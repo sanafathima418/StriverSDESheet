ffbd027eadccccbaa549c07caa8a98836a3aa336
Sana Fathima <sana.fathima@colorado.edu>
2022-12-27 19:08:06 -0700

    Arrays

---
+++ Array/stock_buy_sell_2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:       
+        # Approach: Array Traversal to find minima and maxima in one pass such that maxima occurs after minima
+        # As soon as maxima found, update profit
+        # TC: O(N)
+        # SC: O(1)
+        
+        # Store maxima, minima and profit
+        max_min_set = [prices[0]]*2
+        profit = 0
+        
+        # Traverse over prices array
+        for i in range(1,len(prices)):
+            if prices[i] < max_min_set[0]:
+                # 1. If minima, update max and min
+                 max_min_set = [prices[i]]*2
+            elif prices[i] > max_min_set[1]:
+                # 2. If current price is greater than max, update only max
+                max_min_set[1] = prices[i]
+                # 3. Update profit nonetheless
+                profit += max_min_set[1] - max_min_set[0]
+                # 4. Update minima and maxima to simulate buying on the day sold
+                max_min_set = [prices[i]]*2
+        
+        return profit
